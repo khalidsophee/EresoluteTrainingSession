3f5a6ae1c242473de8d6eb939a78e50036bdf63e
Khalid <khalidsophee@gmail.com>
2020-06-30 16:07:17 +0530

    changes

---
+++ app/src/main/java/com/example/viewbindingpractice/View/FewaPickActivity.kt
@@ -0,0 +1,12 @@
+package com.example.viewbindingpractice.View
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.viewbindingpractice.R
+
+class FewaPickActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_fewa_pick)
+    }
+}
