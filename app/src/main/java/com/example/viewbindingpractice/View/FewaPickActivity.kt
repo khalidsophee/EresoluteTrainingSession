3439eff73839ca699476ac5e2929059ce75bf1b3
Khalid <khalidsophee@gmail.com>
2020-07-01 15:38:15 +0530

    MVVM Enabled ,Live data Enabled

--- app/src/main/java/com/example/viewbindingpractice/View/FewaPickActivity.kt
+++ app/src/main/java/com/example/viewbindingpractice/View/FewaPickActivity.kt
@@ -3,8 +3,10 @@ package com.example.viewbindingpractice.View
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.LinearLayout
+import android.widget.Toast
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import androidx.recyclerview.widget.GridLayoutManager
@@ -20,33 +22,33 @@ import com.google.android.material.bottomsheet.BottomSheetDialog
 class FewaPickActivity : AppCompatActivity() {
 
 
-  private lateinit var pickBinding: ActivityFewaPickBinding
+    private lateinit var pickBinding: ActivityFewaPickBinding
     private lateinit var model: BeneficiaryViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         pickBinding = ActivityFewaPickBinding.inflate(layoutInflater)
         val view = pickBinding.root
         setContentView(view)
-        model = ViewModelProviders.of(this).get(BeneficiaryViewModel::class.java)
-
-        model.getBeneficiary().observe(this,
-            Observer { t ->
-                val rec = findViewById<RecyclerView>(R.id.benificiary_recyclerview_id)
-                rec.layoutManager = LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
-                val adapter = BeneficiaryAdapter(t,this)
-                rec.adapter = adapter
-            })
-
-
-
     }
-
 
     fun ShowList(view: View) {
         val d = BottomSheetDialog(this)
-        val bottomsheet = layoutInflater.inflate(R.layout.submit_dialog, null)
+        val bottomsheet = layoutInflater.inflate(R.layout.bottom_sheet, null)
         d.setContentView(bottomsheet)
         d.show()
+        model = ViewModelProviders.of(this).get(BeneficiaryViewModel::class.java)
+
+    model.getBeneficiary().observe(this,
+            Observer { t ->
+                val rec = bottomsheet.findViewById<RecyclerView>(R.id.benificiary_recyclerview_id)
+                rec.layoutManager = LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
+                val adapter = BeneficiaryAdapter(t,this)
+                Log.d("hello",t.toString())
+                Toast.makeText(this,t.toString(),Toast.LENGTH_LONG).show()
+                rec.adapter = adapter
+            })
+
     }
 
 }
--- app/src/main/java/com/example/viewbindingpractice/ViewModel/BeneficiaryAdapter.kt
+++ app/src/main/java/com/example/viewbindingpractice/ViewModel/BeneficiaryAdapter.kt
@@ -1,6 +1,7 @@
 package com.example.viewbindingpractice.ViewModel
 
 import android.content.Context
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -9,17 +10,19 @@ import android.widget.TextView
 import androidx.cardview.widget.CardView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.viewbindingpractice.Model.BeneficiaryModel
+import com.example.viewbindingpractice.R
 
-class BeneficiaryAdapter(val Beneficairylist: List<BeneficiaryModel>,
-                       val context: Context
+class BeneficiaryAdapter(val Beneficairylist: List<BeneficiaryModel>, val context: Context
 ) : RecyclerView.Adapter<BeneficiaryAdapter.ViewHolder>() {
 
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder
     {
         val l: LayoutInflater = LayoutInflater.from(parent.context)
-        val view: View =l.inflate(R.layout.bottomsheet_view,parent,false)
+        val view=l.inflate(R.layout.bottomsheet_view,parent,false)
         return ViewHolder(view)
+
+        Log.d("am adapter",Beneficairylist.toString())
     }
 
     override fun getItemCount(): Int {
@@ -27,21 +30,20 @@ class BeneficiaryAdapter(val Beneficairylist: List<BeneficiaryModel>,
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        holder.bind(Beneficairylist[position],
+        holder.bind(Beneficairylist[position])
            // View.OnClickListener { listener.onItemClick(position) },DistrictImages[position])
     }
 
     class ViewHolder(itemView: View): RecyclerView.ViewHolder(itemView)
     {
-        val beneficiary_name: TextView =itemView.findViewById(R.id.beneficiary_name) as TextView
-        private var ivDistrict = itemView.findViewById<ImageView>(R.id.beneficiary_account) as TextView
-       // val card=itemView.findViewById<CardView>(R.id.card)
-
+        val beneficiaryname: TextView =itemView.findViewById(R.id.beneficiary_name) as TextView
+        val beneficiaryaccount: TextView =itemView.findViewById(R.id.beneficiary_account) as TextView
 
         fun bind(beneficiaryModel: BeneficiaryModel)
         {
-            beneficiary_name.text=beneficiaryModel.Bname
-            beneficiary_name.text=beneficiaryModel.accountnumber
+
+            beneficiaryname.text=beneficiaryModel.Bname
+            beneficiaryaccount.text=beneficiaryModel.accountnumber.toString()
             //ivDistrict.setBackgroundResource(imgs.resIcon)
                 //card.setOnClickListener(listener)
         }
